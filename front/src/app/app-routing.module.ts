7d6d254cfa8b1e6b40e9ea5332b18a25e8533be0
glaucia86 <glaucia_lemos86@hotmail.com>
2019-02-15 01:47:47 -0200

    Definindo as rotas dos componentes criados no arquivo 'app-routing.module.ts'

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmpAddComponent } from './emp-add/emp-add.component';
+import { EmpGetComponent } from './emp-get/emp-get.component';
+import { EmpEditComponent } from './emp-edit/emp-edit.component';
 
-const routes: Routes = [];
+// Aqui eu defino as rotas de cada componente criado:
+const routes: Routes = [
+  { path: 'funcionario/create', component: EmpAddComponent },
+  { path: 'funcionario/edit/:id', component: EmpEditComponent },
+  { path: 'funcionario', component: EmpGetComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
