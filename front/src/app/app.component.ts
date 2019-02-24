b04107f691ed635c862797a26df94dd18c82ed5c
glaucia86 <glaucia_lemos86@hotmail.com>
2019-02-24 14:05:08 -0300

    Adicionando o ng2-slim-loading-bar na aplicação

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { SlimLoadingBarService } from 'ng2-slim-loading-bar';
+import { NavigationCancel,
+          Event,
+          NavigationEnd,
+          NavigationError,
+          NavigationStart,
+          Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front';
+  constructor(private _loadingBar: SlimLoadingBarService, private _router: Router) {
+    this._router.events.subscribe((event: Event) => {
+      this.navigationInterceptor(event);
+    });
+  }
+
+  // Método para lidar com as ações do 'loading' da Navegação:
+  private navigationInterceptor(event: Event): void {
+    if (event instanceof NavigationStart) {
+      this._loadingBar.start();
+    }
+    if (event instanceof NavigationEnd) {
+      this._loadingBar.complete();
+    }
+    if (event instanceof NavigationCancel) {
+      this._loadingBar.stop();
+    }
+    if (event instanceof NavigationError) {
+      this._loadingBar.stop();
+    }
+  }
 }
