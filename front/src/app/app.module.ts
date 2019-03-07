5ee9977c4862c37187cee03e0ce8a18c1e2ab7df
glaucia86 <glaucia_lemos86@hotmail.com>
2019-03-07 20:16:49 -0300

    Criando o arquivo EmpresaService.

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 import { AngularFontAwesomeModule  } from 'angular-font-awesome';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmpAddComponent } from './emp-add/emp-add.component';
 import { EmpEditComponent } from './emp-edit/emp-edit.component';
 import { EmpGetComponent } from './emp-get/emp-get.component';
+import { EmpresaService } from './empresa.service';
 
 @NgModule({
   declarations: [
@@ -22,9 +24,10 @@ import { EmpGetComponent } from './emp-get/emp-get.component';
     AppRoutingModule,
     SlimLoadingBarModule,
     AngularFontAwesomeModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [EmpresaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
