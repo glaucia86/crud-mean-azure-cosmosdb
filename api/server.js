79c5cd277b97b7e629190d8ca6259057a40b1e22
glaucia86 <glaucia_lemos86@hotmail.com>
2019-06-22 20:55:22 -0300

    Atualização do arquivo 'server.js'

--- api/server.js
+++ api/server.js
@@ -8,38 +8,6 @@
  * Author: Glaucia Lemos
  */
 
-// ==> Exemplo sem realizar o SOLID:
-
-/* const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const cors = require('cors');
-const path = require('path');
-
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-app.use(morgan('dev'));
-app.use(cors());
-
-const port = process.env.PORT || 8000;
-
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json({ message: 'Entrei aqui!' });
-});
-
-app.use('/api', router);
-
-app.listen(port);
-console.log("Aplicação executando na porta ", port); */
-
-// ==> Exemplo usando o SOLID:
-
 const app = require('./src/app');
 
 const port = process.env.PORT || 8000;
